7d64c36c9b81f38aad1e6a405a018dc0bdd310a2
Jack <jack.downes91@gmail.com>
2017-12-13 13:43:28 +0800

    add procedure addcust

--- Database/create_procs.sql
+++ Database/create_procs.sql
@@ -1,20 +1,31 @@
-DROP PROCEDURE IF EXISTS aaddCust;
-
+DROP PROCEDURE IF EXISTS addCust;
 DELIMITER $$
-
-CREATE PROCEDURE assignProj (
+CREATE PROCEDURE addCust(
     n CHAR(50),
     e CHAR(50)
     )
     COMMENT 'Get next Cust ID and add new customer'
     BEGIN
-        IF EXISTS (SELECT custID FROM Cust) THEN
-            @id = SELECT MAX(custID)+1 FROM Cust;
+        DECLARE c INT(5);
+        SET c = (SELECT COUNT(*) FROM Cust);
+        IF (c > 0 && c < 10000) THEN 
+            SET @id = (SELECT (MAX(custID)+1) FROM Cust);
             INSERT INTO Cust (custID, name, email)
             VALUES (@id, n, e);
+        ELSE IF (c == 0)
+            INSERT INTO Cust (custID, name, email) VALUES (0001, n, e); 
         ELSE 
-            INSERT INTO Cust (custID, name, email)
-            VALUES (@id, n, e);
-            ;
+            SELECT 'customer table exceeds limit of 9999 entries'
+        END IF;
     END $$
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS test;
+DELIMITER $$
+CREATE PROCEDURE test()
+    COMMENT 'Test a single line select statement proc'
+    BEGIN
+        SELECT 'test';
+    END $$
+DELIMITER ;
+
