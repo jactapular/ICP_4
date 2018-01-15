7dfcf8909610602deb5e489c34759185423c5244
Jack <jack.downes91@gmail.com>
2018-01-15 15:42:31 +0800

    update webhook.py

--- Scripts/webhook.py
+++ Scripts/webhook.py
@@ -6,7 +6,9 @@ from mysql.connector import MySQLConnection, Error
 #from configparser import ConfigParser
 #from python_mysql_dbconfig import read_db_config
 # from datetime import date, datetime, timedelta, time
-# from time import strftime, sleep
+
+###### FOR TESTING ######
+from time import strftime, sleep
     try:
     db_config = {
         'host' = '10.0.10.220:3306'
@@ -14,15 +16,27 @@ from mysql.connector import MySQLConnection, Error
         'user' = 'trans'
         'password' = 'GreenX0123'
     }
-    
-    t = request.view_args["Time"]
-    
+            # # unit ID
+            # uID = request.view_args["Time"]
+            # # time
+            # time = request.view_args["DevAddr"]
+            # # temp 
+            # tmp = 
+            # #humidity
+            # hum =
+    # unit ID
+    uID = 0x00540006;
+    # time
+    time = strftime('%Y-%m-%d %H:%M:%S')
+    # temp = 26
+    tmp = 0x1A
+    #humidity = 37
+    hum = 25
         conn = MySQLConnection(**db_config)
-        if request.method == 'GET':
-            cursor = conn.cursor()
-            cursor.execute("CALL addRead(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(1, t, 1, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
-            print "add reading at: ", t
-            conn.commit()
+        cursor = conn.cursor()
+        cursor.execute("CALL addRead(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(uID, time, 1, 1, int(tmp, 16), int(hum, 16), 0.0, 0.0, 0.0, 0.0))
+        print "add reading at: ", t
+        conn.commit()
     except Error as error:
         print(error)
 
