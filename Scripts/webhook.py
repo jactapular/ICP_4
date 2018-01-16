2086c2b1c3601da8a5a6cf08f42c2eaf51615087
jack-student <14843574@student.curtin.edu.au>
2018-01-16 13:12:21 +0800

    update webhook.py for deployment

--- Scripts/webhook.py
+++ Scripts/webhook.py
@@ -8,23 +8,25 @@ conn = MySQLConnection( host = '10.0.10.220',
                             password = 'GreenX0123')
 curs = conn.cursor()
 
-    # # unit ID
-    # # uID = request.data["Time"]
-    # # time
-    # time = request.view_args["DevAddr"]
-    # # temp 
-    # tmp = request.data["DevEUI_uplink"."payload_hex"."mydata[0]"]
-    # tmp = int(tmp, 16)
-    # #humidity
-    # hum = request.data["DevEUI_uplink"."payload_hex"."mydata[1]"]
-    # hum = int(hum, 16)
     # unit ID
-uID = '0x00540006';
-    # time
-time = strftime('%Y-%m-%d %H:%M:%S')
-tmp = 37.01
+uID = request.data["DevAddr"]
+
+    # temp 
+tmp = request.data["DevEUI_uplink"]["payload_hex"]["mydata"][0]
+    # tmp = int(tmp, 16)
+
+    # #humidity
+hum = request.data["DevEUI_uplink"]["payload_hex"]["mydata"][0]
+    # hum = int(hum, 16)
+    
+
+#for testing
+#uID = '0x00540006';
+# time
+#time = strftime('%Y-%m-%d %H:%M:%S')
+#tmp = 37.01
 #tmp = 0x1A
-hum = 52.01
+#hum = 52.01
 #hum = 0x25
 
 add_read = ("CALL addRead(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
