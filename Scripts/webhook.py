c47832d531b608f1e62d81d74e12d246fbded984
jack-student <14843574@student.curtin.edu.au>
2018-01-16 12:36:21 +0800

    update webhook.py

--- Scripts/webhook.py
+++ Scripts/webhook.py
@@ -1,14 +1,6 @@
 from mysql.connector import MySQLConnection, Error
-###### NOT REQUIRED ######
-#from configparser import ConfigParser
-#from python_mysql_dbconfig import read_db_config
-# from datetime import date, datetime, timedelta, time
-
 ###### FOR TESTING ######
 from time import strftime, sleep
-
-#try:
-
 
 conn = MySQLConnection( host = '10.0.10.220',
                             database = 'ICP',
@@ -30,25 +22,18 @@ curs = conn.cursor()
 uID = '0x00540006';
     # time
 time = strftime('%Y-%m-%d %H:%M:%S')
-    # temp = 26
-tmp = 0x1A
-    #humidity = 37
-hum = 0x25
-
-#add_read = ("CALL addCust('testy Mcgee', 'test@email.com')")
-#data = ('0123', time, 1, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+tmp = 37.01
+#tmp = 0x1A
+hum = 52.01
+#hum = 0x25
 
 add_read = ("CALL addRead(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
-data = (uID, time, 1, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 
-#add_read = ("CALL addRead('0x00540006', '2017-12-19 15:22:00', 1, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)")
+data = (uID, time, 1, 1, tmp, hum, 0.0, 0.0, 0.0, 0.0)
 
 curs.execute(add_read, data)
 
 conn.commit()
-#except Error as error:
-#print(error)
 
-#finally:
 curs.close()
 conn.close()
