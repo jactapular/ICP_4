4a1ed5733c020942472da58e784ec23f2c2f6674
Jack <jack.downes91@gmail.com>
2018-01-17 14:12:31 +0800

    update webhook.py

--- Scripts/webhook_test.py
+++ Scripts/webhook_test.py
@@ -1,34 +1,25 @@
-from mysql.connector import MySQLConnection
+from mysql.connector import MySQLConnection, Error
+###### FOR TESTING ######
 from time import strftime, sleep
 
 conn = MySQLConnection( host = '10.0.10.220',
                             database = 'ICP',
                             user = 'tran',
                             password = 'GreenX0123')
-curs = conn.cursor()
+curs = conn.cursor() 
 
-# unit ID
-uID = request.data["DevEUI_uplink"]["DevAddr"]
-
+#for testing
+uID = '0x00540006';
 # time
 time = strftime('%Y-%m-%d %H:%M:%S')
-
-# temp 
-tmpH = request.data["DevEUI_uplink"]["payload_hex"]
-tmpH = tmpH[0:2]
-tmp = int(tmpH, 16)
-
-
-#humidity
-humH = request.data["DevEUI_uplink"]["payload_hex"]
-humH = humh[2:2]
-hum = int(humH, 16)
-
-
+tmp = 37.01
+#tmp = 0x1A
+hum = 52.01
+#hum = 0x25
 
 add_read = ("CALL addRead(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
 
-data = (uID, time, 1, 1, float(tmp), float(hum), 0.0, 0.0, 0.0, 0.0)
+data = (uID, time, 1, 1, tmp, hum, 0.0, 0.0, 0.0, 0.0)
 
 curs.execute(add_read, data)
 
